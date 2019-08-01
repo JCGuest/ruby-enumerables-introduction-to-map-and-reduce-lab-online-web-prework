2d57e66e478992a10bf378badd2589b75720f46b
John Guest <jcguest90@gmail.com>
2019-08-01 06:22:11 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,19 @@
-# My Code here....
+def map_to_negativize(source_array)
+  neg = []
+  count = 0 
+  while count < source_array.length do
+   neg << (source_array[count] * -1)
+   count += 1
+ end
+ neg
+end
+
+def map_to_change(source_array)
+  new = []
+  i = 0 
+  while i < source_array.length do 
+    new.push (source_array[count])
+    count += -1
+end
+new
+end
